9e5624c705b013c3b6eec1a6e06436f40cbf61a0
felixlopz <felixlopzd@gmail.com>
2020-02-29 20:02:55 -0400

    fixing typo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -90,7 +90,7 @@ const Icon = styled.img`
 const Navigation = () => {
 
 	const [isOpen, setIsOpen] = useState(false);
-	const isMatch = useMatch('/inner-page);
+	const isMatch = useMatch('/inner-page');
 
   return (
       <NavWrapper isOpen={isOpen}>
